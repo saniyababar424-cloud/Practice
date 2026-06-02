9230aa610e06d24caadc795595ab0d7976d52801
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-02 23:12:58 +0530

    Assignment 2 - Question 1: Display Cafe Menu Using Java

---
+++ cafeMenu.java
@@ -0,0 +1,18 @@
+public class cafeMenu {
+    public static void main(String[]args){
+        System.out.println("---------------------------------------");
+        System.out.println("         WELCOME TO JAVA CAFE    ");
+        System.out.println("---------------------------------------");
+        System.out.println("  +-----------------------------------+");
+        System.out.println("  | Item                    Price     |");
+        System.out.println("  +----------------------------------+");
+        System.out.println("  |Tea                       $10      |");
+        System.out.println("  |Coffee                    $20      |");
+        System.out.println("  |Sandwich                  $25      |");
+        System.out.println("  |Burger                    $30      |");
+        System.out.println("  |Pizza                     $150     |");
+        System.out.println("  +-----------------------------------+");
+        System.out.println("Today's Special : Veg Burger Combo");
+    System.out.println("Thank You For Visiting Java Cafe !");
+    }
+}
