e22ab363d489b4b4ed5935e00e8d54bd2a5d24cb
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-03 09:13:45 +0530

    Cafe Menu Details

--- cafeMenu.java
+++ cafeMenu.java
@@ -1,18 +1,21 @@
 public class cafeMenu {
     public static void main(String[]args){
-        System.out.println("---------------------------------------");
-        System.out.println("         WELCOME TO JAVA CAFE    ");
-        System.out.println("---------------------------------------");
-        System.out.println("  +-----------------------------------+");
-        System.out.println("  | Item                    Price     |");
-        System.out.println("  +----------------------------------+");
-        System.out.println("  |Tea                       $10      |");
-        System.out.println("  |Coffee                    $20      |");
-        System.out.println("  |Sandwich                  $25      |");
-        System.out.println("  |Burger                    $30      |");
-        System.out.println("  |Pizza                     $150     |");
-        System.out.println("  +-----------------------------------+");
+        System.out.println("===================================================================");
+        System.out.println("                    WELCOME TO JAVA CAFE    ");
+        System.out.println("===================================================================");
+        System.out.println("");
+        System.out.println(" +----------------------------------------------------------------+");
+        System.out.println(" | Item                    Price                                  |");
+        System.out.println(" +----------------------------------------------------------------+");
+        System.out.println(" |Tea                       $2                                    |");
+        System.out.println(" |Coffee                    $3                                    |");
+        System.out.println(" |Sandwich                  $5                                    |");
+        System.out.println(" |Burger                    $7                                    |");
+        System.out.println(" |Pizza                     $10                                   |");
+        System.out.println(" +----------------------------------------------------------------+");
+        System.out.println("");
         System.out.println("Today's Special : Veg Burger Combo");
-    System.out.println("Thank You For Visiting Java Cafe !");
+        System.out.println("");
+    System.out.println("Thank You For Visiting Java Cafe!");
     }
 }
