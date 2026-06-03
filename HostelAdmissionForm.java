fc35df2a25496c520356cc9b781e228fc9b17e04
saniyababar424-cloud <saniyababar424@gmail.com>
2026-06-03 09:12:14 +0530

    Hostel Admission Form

---
+++ HostelAdmissionForm.java
@@ -0,0 +1,21 @@
+public class HostelAdmissionForm {
+    public static void main(String args[]){
+        System.out.println("=============================================");
+        System.out.println("      Hostel Admission Form                  ");
+        System.out.println("=============================================");
+        System.out.println("Student Name          : Babar Saniya Sachin");
+        System.out.println("Father's Name           : Babar Sachin Ragunath");
+        System.out.println("Mother's Name           : Babar Anita Sachin");
+        System.out.println("Date of Birth           : 09/07/2007");
+        System.out.println("Gender                  : Femal ");
+        System.out.println("Class                   : CSE(TY)");
+        System.out.println("Roll No                 : 06");
+        System.out.println("Hostel No               : 1");
+        System.out.println("Hostel Name             :Ganga");
+        System.out.println("Room No                 : 18 ");
+        System.out.println("Mobile No               : 9359xxxxxx");
+        System.out.println("Parent Mobile No        : 7875xxxxxx");
+        System.out.println("Address                 : Sangli,Maharashtra ");
+
+    }
+}
